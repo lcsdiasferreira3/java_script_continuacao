5ff14b1d81125367089e57f9564978ccfa75605d
lcsdiasferreira3 <lcsdferreira3@gmail.com>
2018-08-23 22:27:20 -0300

    Java script com foto e fonte

--- js/funcoes.js
+++ js/funcoes.js
@@ -23,7 +23,7 @@ function naoolcultarElemento(){
 	
 }
 function calcular(){
-	document.getElementById("demo").innerHTML = parseFloat(document.getElementById("num1").value) + parseFloat(document.getElementById("num2").value);
+var total = parseFloat(document.getElementById("num1").value) + parseFloat(document.getElementById("num2").value); window.alert(total);
 }
 	
 	
