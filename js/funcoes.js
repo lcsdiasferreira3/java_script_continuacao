70089bc89ab3b32e7086c9628b5df40465bd5f0f
lcsdiasferreira3 <lcsdferreira3@gmail.com>
2018-08-23 22:20:28 -0300

    Java script com foto e fonte

--- js/funcoes.js
+++ js/funcoes.js
@@ -6,4 +6,27 @@ function imagemTroca() {
 }
 function trocaFonte() {
 	document.getElementById("demo").style.fontSize="100px";
-}	
+}
+function trocaFonteMaior() {
+	document.getElementById("demo").style.fontSize="35px";
+	
+}
+function trocaFontemenor(){
+	document.getElementById("demo").style.fontSize="16px";
+	
+}
+function OlcutarElemento(){
+	document.getElementById("demo").style.display="none";
+}
+function naoolcultarElemento(){
+	document.getElementById("demo").style.display="block";
+	
+}
+function calcular(){
+	document.getElementById("demo").innerHTML = parseFloat(document.getElementById("num1").value) + parseFloat(document.getElementById("num2").value);
+}
+	
+	
+	
+	
+	
